19a1605f9cbd8fe540e628d801f5350f8a5a3430
zheng920 <xuan.zheng2011@gmail.com>
2014-04-22 13:36:35 -0700

    modified for EMR

--- WordIndexer/src/index/IndexRetrieval.java
+++ WordIndexer/src/index/IndexRetrieval.java
@@ -1,19 +1,16 @@
 package index;
 
-import hash.SHA1Partition;
 
 import java.util.ArrayList;
 
-import storage.BerkeleyDB;
-import storage.WordDocTFEntry;
 
 public class IndexRetrieval {
 
-	public static ArrayList<WordDocTFEntry> retrieve(String word){
-		int workerIndex = SHA1Partition.getWorkerIndex(word);
-		//TODO connect other workers according to the worker index, through servlets
-		BerkeleyDB db = IndexDriver.db;
-		return db.getTFIDF(word);
-	}
+//	public static ArrayList<WordDocTFEntry> retrieve(String word){
+//		int workerIndex = SHA1Partition.getWorkerIndex(word);
+//		//TODO connect other workers according to the worker index, through servlets
+//		BerkeleyDB db = IndexDriver.db;
+//		return db.getTFIDF(word);
+//	}
 
 }
