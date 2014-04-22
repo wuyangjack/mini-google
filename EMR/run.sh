37254bf8bfa1684dfa37a6c093999791f58efa90
Yang (Jack) Wu <yangwu6@cis.upenn.edu>
2014-04-21 23:03:51 -0400

    Added EMR tools.

--- EMR/run.sh
+++ EMR/run.sh
@@ -1,1 +1,20 @@
 #!/bin/sh
+
+ROOT="/home/cis455/MiniGoogle/EMR"
+CLI=$ROOT"/bin"
+BUCKET="cis555-project-test"
+
+case "$1" in
+	SampleEMR)
+		cd $CLI
+		./elastic-mapreduce --create --name "Test custom JAR" \
+		  	--jar s3n://elasticmapreduce/samples/cloudburst/cloudburst.jar \
+		    --arg s3n://elasticmapreduce/samples/cloudburst/input/s_suis.br \
+		    --arg s3n://elasticmapreduce/samples/cloudburst/input/100k.br \
+		    --arg "s3n://"$BUCKET"/cloud" \
+		    --arg 36 --arg 3 --arg 0 --arg 1 --arg 240 --arg 48 --arg 24 \
+		    --arg 24 --arg 128 --arg 16
+		;;
+	*)
+		;;
+esac
