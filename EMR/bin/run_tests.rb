cf3d1caf2432fa56f26b61a83941ac4632e0c344
Yang (Jack) Wu <yangwu6@cis.upenn.edu>
2014-04-23 12:54:09 -0700

    Delete sensitive.

--- EMR/bin/run_tests.rb
+++
@@ -1,8 +0,0 @@
-#
-# Copyright 2008-2010 Amazon.com, Inc. or its affiliates.  All Rights Reserved.
-
-$LOAD_PATH << File.dirname(__FILE__)
-
-require 'tests/commands_test'
-
-
--- EMR/run.sh
+++ EMR/run.sh
@@ -1,14 +1,16 @@
 #!/bin/sh
 
 ROOT="/home/cis455/MiniGoogle/EMR"
-CLI=$ROOT"/bin"
 BUCKET="s3://cis555-project-test"
 USER="wuyangjack1991@gmail.com"
 
+# Install AWS API
+# Install EMR API
+# Add EMR bin path to $PATH
+
 case "$1" in
 	JobSample)
-		cd $CLI
-		./elastic-mapreduce --create --name "Test custom JAR" \
+		elastic-mapreduce --create --name "Test custom JAR" \
 		  	--jar s3n://elasticmapreduce/samples/cloudburst/cloudburst.jar \
 		    --arg s3n://elasticmapreduce/samples/cloudburst/input/s_suis.br \
 		    --arg s3n://elasticmapreduce/samples/cloudburst/input/100k.br \
@@ -17,36 +19,34 @@ case "$1" in
 		    --arg 24 --arg 128 --arg 16
 		;;
 	JobPageRank)
-		cd $CLI
 		JAR=project-pagerank.jar
 		CLASS="cis455.project.PageRankDriver"
 		INPUT=$BUCKET"/input"
 		OUTPUT=$BUCKET"/output"
 		ITERATION=3
 		MODE="emr"
-		./elastic-mapreduce --create --name "Test PageRank JAR" \
-		--jar $BUCKET"/"$JAR \
-		--arg $CLASS \
-		--arg $MODE \
-		--arg $INPUT \
-		--arg $OUTPUT \
-		--arg $ITERATION
+		elastic-mapreduce --create --name "Test PageRank JAR" \
+			--jar $BUCKET"/"$JAR \
+			--arg $CLASS \
+			--arg $MODE \
+			--arg $INPUT \
+			--arg $OUTPUT \
+			--arg $ITERATION
 		;;
 	JobCrawler)
-		cd $CLI
 		JAR=HadoopCrawler.jar
 		CLASS="hadoopcrawler.CrawlerDriver"
 		INPUT=$BUCKET"/data/level/urlfile"
 		OUTPUT=$BUCKET"/data/level"
 		DATABASE="berkeleydb"
 		SIZE=10
-		./elastic-mapreduce --create --name "Test Crawler JAR" \
-		--jar $BUCKET"/"$JAR \
-		--arg $CLASS \
-		--arg $INPUT \
-		--arg $OUTPUT \
-		--arg $DATABASE \
-		--arg $SIZE
+		elastic-mapreduce --create --name "Test Crawler JAR" \
+			--jar $BUCKET"/"$JAR \
+			--arg $CLASS \
+			--arg $INPUT \
+			--arg $OUTPUT \
+			--arg $DATABASE \
+			--arg $SIZE
 		;;
 	DeleteFile)
 		aws s3 rm $BUCKET/$2
@@ -64,8 +64,7 @@ case "$1" in
 		aws s3 cp $2 $BUCKET"/" --grants full=emailaddress=$USER
 		;;
 	Verify)
-		cd $CLI
-		./elastic-mapreduce --version
+		elastic-mapreduce --version
 		;;
 	*)
 		;;
