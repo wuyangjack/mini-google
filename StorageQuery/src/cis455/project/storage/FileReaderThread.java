da9ad6d7dabff8d97f87c9ebe1e29dbcae644021
zheng920 <xuan.zheng2011@gmail.com>
2014-05-03 20:29:56 -0700

    blocking queue

--- StorageQuery/src/cis455/project/storage/FileReaderThread.java
+++ StorageQuery/src/cis455/project/storage/FileReaderThread.java
@@ -5,58 +5,65 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.concurrent.BlockingQueue;
 
 import cis455.project.hash.SHA1Partition;
 
 public class FileReaderThread extends Thread{
 	
-	private File file;
+	private BlockingQueue<File> queue = null;
 	Storage storage = Storage.getInstance();
 	
-	public FileReaderThread(File file){
-		this.file = file;
+	public FileReaderThread(BlockingQueue<File> queue){
+		this.queue = queue;
 	}
 	
 	@Override
 	public void run(){
 		BufferedReader br = null;
+		SHA1Partition.setRange(StorageDumper.NODE_NUMBER);
 		try {
-			SHA1Partition.setRange(StorageDumper.NODE_NUMBER);
-			br = new BufferedReader(new FileReader(file));
-			String line = null;
-			String key = "";
-			String value = "";
-			String SHAkey = "";
-			
-			while((line = br.readLine()) != null){
-				
-				String[] data = line.trim().split("\t", 2);
-				key = data[0];
-				value = data[1];
-				if (data.length != 2) {
-					System.out.println("Illegal");
+			while(true){
+				File file = this.queue.take();
+				if(file.compareTo(StorageDumper.poison) == 0){
+					break;
 				}
-				if(StorageDumper.dbName.equals("tablePageRank")){
-					SHAkey = key;
-				}
-				else{
-					//SHA key = docId
-					SHAkey = value.split("\t")[0];
-				}
+				br = new BufferedReader(new FileReader(file));
+				String line = null;
+				String key = "";
+				String value = "";
+				String SHAkey = "";
 
+				while((line = br.readLine()) != null){
 
-				System.out.println(file.getPath() + ":");
-				System.out.print("\"" + data[0] + "\": ");
-				if(!SHAkey.equals("") && (SHA1Partition.getWorkerIndex(SHAkey) == StorageDumper.NODE_INDEX)){
-					
-					System.out.println("Save to " + StorageDumper.dbName);
-					if(!storage.put(StorageDumper.dbName, key, value)){
-						Log.error("bug whern putting into DB + key + value: " + StorageDumper.dbName
-								+ " + " + data[0]+ " + " + line);
-						throw new Exception("DB Exception occured while putting index data!");
+					String[] data = line.trim().split("\t", 2);
+					key = data[0];
+					value = data[1];
+					if (data.length != 2) {
+						System.out.println("Illegal");
 					}
-				} else {
-					System.out.println("Skip");
+					if(StorageDumper.dbName.equals("tablePageRank")){
+						SHAkey = key;
+					}
+					else{
+						//SHA key = docId
+						SHAkey = value.split("\t")[0];
+					}
+
+
+					System.out.println(file.getPath() + ":");
+					System.out.print("\"" + data[0] + "\": ");
+					if(!SHAkey.equals("") && (SHA1Partition.getWorkerIndex(SHAkey) == StorageDumper.NODE_INDEX)){
+
+						System.out.println("Save to " + StorageDumper.dbName);
+						if(!storage.put(StorageDumper.dbName, key, value)){
+							Log.error("bug whern putting into DB + key + value: " + StorageDumper.dbName
+									+ " + " + data[0]+ " + " + line);
+							throw new Exception("DB Exception occured while putting index data!");
+						}
+					} else {
+						System.out.println("Skip");
+					}
 				}
 			}
 		} catch (Exception e) {
--- StorageQuery/src/cis455/project/storage/StorageDumper.java
+++ StorageQuery/src/cis455/project/storage/StorageDumper.java
@@ -1,6 +1,8 @@
 package cis455.project.storage;
 
 import java.io.File;
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.LinkedBlockingQueue;
 
 public class StorageDumper {
 	
@@ -10,6 +12,7 @@ public class StorageDumper {
 		NODE_NUMBER = Integer.parseInt(args[2]);
 		NODE_INDEX = Integer.parseInt(args[3]);
 		dbName = args[4];
+		dumpThreadNum = Integer.parseInt(args[5]);
 		Storage.setEnvPath(dbDir, false);
 		dump();
 	}
@@ -17,17 +20,34 @@ public class StorageDumper {
 	public static int NODE_INDEX = 1;
 	public static int NODE_NUMBER = 5;
 	public static String dbName = null;
-	public static String  dataPath = null;;
+	public static String  dataPath = null;
+	public static int dumpThreadNum = 1;
+	public static File poison = new File("poison");
 
 	public static void dump(){
 		File dir = new File(dataPath);
 		File[] fileList = dir.listFiles();
-		Thread[] threads = new Thread[fileList.length];
+		
+		BlockingQueue<File> entryQ = new LinkedBlockingQueue<File>();
+		
+		try {
+			for(int i = 0; i < fileList.length; i++){
+				entryQ.put(fileList[i]);
+			}
+			for(int i = 0; i < StorageDumper.dumpThreadNum; i++){
+				entryQ.put(StorageDumper.poison);
+			}
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		Thread[] threads = new Thread[StorageDumper.dumpThreadNum];
 		
 		Log.info("Start dumping into DB - " + dbName + " , thread number is " + fileList.length);
 		
-		for(int i = 0; i < fileList.length; i++){
-			threads[i] = new FileReaderThread(fileList[i]);
+		for(int i = 0; i < threads.length; i++){
+			threads[i] = new FileReaderThread(entryQ);
 			threads[i].start();
 		}
 	}
