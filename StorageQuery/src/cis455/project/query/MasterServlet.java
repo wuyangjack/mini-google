a34bfd786413661e74feaf9fad404a9149dca03b
Yang (Jack) Wu <yangwu6@cis.upenn.edu>
2014-05-04 16:57:43 -0400

    Compile & deploy.

--- StorageQuery/src/cis455/project/query/MasterServlet.java
+++ StorageQuery/src/cis455/project/query/MasterServlet.java
@@ -13,10 +13,10 @@ public class MasterServlet extends HttpServlet {
 	@Override
 	public void init() throws ServletException {
 		String[] servers = new String[]{
-				"ec2-50-16-82-61.compute-1.amazonaws.com:8080", 
+				"ec2-50-16-82-61.compute-1.amazonaws.com:8080" /*, 
 				"ec2-54-221-151-217.compute-1.amazonaws.com:8080",
 				"ec2-54-237-211-134.compute-1.amazonaws.com:8080",
-				"ec2-54-82-187-46.compute-1.amazonaws.com:8080"
+				"ec2-54-82-187-46.compute-1.amazonaws.com:8080"*/
 		};
 		ServletConfig config = getServletConfig();
 		String pathDatabase = config.getInitParameter(QueryGlobal.initPathDatabase);
