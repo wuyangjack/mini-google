ab9b92f5d0bdfd6a9b6f303ef802af3b7462d00d
Yang (Jack) Wu <yangwu6@cis.upenn.edu>
2014-05-06 10:47:43 -0400

    Deleted old UI for tomcat6.

--- UI/src/cis455/project/storage/StorageGlobal.java
+++
@@ -1,8 +0,0 @@
-package cis455.project.storage;
-
-public class StorageGlobal {
-	public static final String tableFreqTitle = "freqtitle";
-	public static final String tableFreqMeta = "freqmeta";
-	public static final String tableFreqBody = "freqbody";
-	public static final String tablePageRank = "pagerank";
-}
