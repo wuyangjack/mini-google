59f57c69841bb6be04f47241e17be5c8fe7f59ec
Yang (Jack) Wu <yangwu6@cis.upenn.edu>
2014-05-08 11:01:57 -0400

    Refractored UI code.

--- UI/src/uiTest/UIGlobal.java
+++
@@ -1,9 +0,0 @@
-package uiTest;
-
-public class UIGlobal {
-	public static final Integer pageVolume = 10;
-	public static final String delimiterUI = "\t";
-	public static final String CRLF = "\r\n";
-	public static final String searchURL = "http://ec2-54-221-87-56.compute-1.amazonaws.com:8080/master/query?mode=search&query=";
-
-}
