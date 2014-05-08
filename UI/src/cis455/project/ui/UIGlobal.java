0ab7254ef0f1fa97168ff530d99960a29bb38038
Yang (Jack) Wu <yangwu6@cis.upenn.edu>
2014-05-08 14:08:01 -0400

    Compile & deploy servlets.

--- UI/src/cis455/project/ui/UIGlobal.java
+++ UI/src/cis455/project/ui/UIGlobal.java
@@ -7,7 +7,7 @@ public class UIGlobal {
 	public static final String urlMasterSearch = "http://ec2-54-221-87-56.compute-1.amazonaws.com:8080/master/query?mode=search&query=";
 	public static final String jspIndex = "/index.jsp";
 	public static final String jspResult = "/result.jsp";
-	public static final String pathSearch = "/search";
+	public static final String pathSearch = "search";
 	
 	public static final String paraPage = "page";
 	public static final String paraAmazon = "amazon";
